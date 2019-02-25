8e033dd1e8feeb41d15b189584b1a3a0105f2d15
Jonathan Gardner <jonathangardner@jonathans-mbp.home>
2019-02-24 19:57:58 -0500

    small update

--- clear-db.js
+++ clear-db.js
@@ -1,5 +1,12 @@
 // This file clears the Mongo Database
 const mongoose = require('mongoose');
-const conn = mongoose.connection;
+const keys = require('./config/keys');
 
-conn.db.dropDatabase();
+mongoose.connect(keys.MONGO_URI, { useNewUrlParser: true })
+  .then(
+    () => console.log("connected to the database"),
+    err => console.error.bind(console, "database connection error:")
+  );
+mongoose.connection.dropDatabase( () => {
+  process.exit();
+});
